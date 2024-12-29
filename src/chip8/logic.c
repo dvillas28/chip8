3dd2618dc5c89787500b359c405d833d90aa5dc3
dvillas28 <dvillasc@gmail.com>
2024-12-28 23:19:00 -0300

    1 2 3 4 5 6 7 8 ins done

--- src/chip8/logic.c
+++ src/chip8/logic.c
@@ -9,10 +9,62 @@ void op_00E0(ChipContext *ctx)
     }
 }
 
+void op_00EE(ChipContext *ctx)
+{
+    /* return */
+    ctx->SP--;                     // decrement from stack
+    ctx->PC = ctx->stack[ctx->SP]; // pop value
+}
+
 void op_1NNN(ChipContext *ctx, u8 n1, u8 n2, u8 n3)
 {
     /* jump */
     ctx->PC = (n1 << 8) | (n2 << 4) | n3;
+}
+
+void op_2NNN(ChipContext *ctx, u8 n1, u8 n2, u8 n3)
+{
+    /* call subroutine */
+    ctx->stack[ctx->SP] = ctx->PC; // push current instruction to stack
+    ctx->SP++;                     // increment stack pointer
+
+    ctx->PC = (n1 << 8) | (n2 << 4) | n3;
+}
+
+void op_3XNN(ChipContext *ctx, u8 x, u8 n1, u8 n2)
+{
+    /* skip if equal */
+    if (ctx->V[x] == ((n1 << 4) | n2))
+    {
+        ctx->PC += 2;
+    }
+}
+
+void op_4XNN(ChipContext *ctx, u8 x, u8 n1, u8 n2)
+{
+    /* skip if not equal */
+    if (ctx->V[x] != ((n1 << 4) | n2))
+    {
+        ctx->PC += 2;
+    }
+}
+
+void op_5XY0(ChipContext *ctx, u8 x, u8 y)
+{
+    /* skip if Vx == Vy */
+    if (ctx->V[x] == ctx->V[y])
+    {
+        ctx->PC += 2;
+    }
+}
+
+void op_9XY0(ChipContext *ctx, u8 x, u8 y)
+{
+    /* skip if Vx != Vy */
+    if (ctx->V[x] != ctx->V[y])
+    {
+        ctx->PC += 2;
+    }
 }
 
 void op_6XNN(ChipContext *ctx, u8 x, u8 n1, u8 n2)
@@ -26,6 +78,98 @@ void op_7XNN(ChipContext *ctx, u8 x, u8 n1, u8 n2)
     /* add value nn to vx */
     // the flag register vF is NOT affected
     ctx->V[x] += (n1 << 4) | n2;
+}
+
+void op_8XY0(ChipContext *ctx, u8 x, u8 y)
+{
+    /* Vx <- Vy */
+    ctx->V[x] = ctx->V[y];
+}
+
+void op_8XY1(ChipContext *ctx, u8 x, u8 y)
+{
+    /* Vx <- Vx OR Vy */
+    ctx->V[x] |= ctx->V[y];
+}
+
+void op_8XY2(ChipContext *ctx, u8 x, u8 y)
+{
+    /* Vx <- Vx AND Vy */
+    ctx->V[x] &= ctx->V[y];
+}
+
+void op_8XY3(ChipContext *ctx, u8 x, u8 y)
+{
+    /* Vx <- Vx XOR Vy */
+    ctx->V[x] ^= ctx->V[y];
+}
+
+void op_8XY4(ChipContext *ctx, u8 x, u8 y)
+{
+    /* Vx <- Vx + Vy, this will affect the carry flag */
+    u16 sum = ctx->V[x] + ctx->V[y];
+    // if result is greater than 8 bits
+    if (sum > 255U)
+    {
+        ctx->V[0xF] = 1;
+    }
+    else
+    {
+        ctx->V[0xF] = 0;
+    }
+
+    // mantain only the 8 LSB
+    ctx->V[x] = sum & 0xFFU;
+}
+
+void op_8XY5(ChipContext *ctx, u8 x, u8 y)
+{
+    /* Vx <- Vx - Vy, this will affect the carry flag */
+    if (ctx->V[x] > ctx->V[y])
+    {
+        ctx->V[0xF] = 1;
+    }
+    else
+    {
+        ctx->V[0xF] = 0;
+    }
+
+    ctx->V[x] -= ctx->V[y];
+}
+
+void op_8XY7(ChipContext *ctx, u8 x, u8 y)
+{
+    /* Vx <- Vy - Vx, this will affect the carry flag */
+    if (ctx->V[y] > ctx->V[x])
+    {
+        ctx->V[0xF] = 1;
+    }
+    else
+    {
+        ctx->V[0xF] = 0;
+    }
+
+    ctx->V[x] = ctx->V[y] - ctx->V[x];
+}
+
+void op_8XY6(ChipContext *ctx, u8 x, u8 y)
+{
+    /* Vx <- SHR Vx */
+    // TODO: add via configuration mode this before Vx <- Vy
+
+    // store the LSB in VF
+    ctx->V[0xF] = ctx->V[x] & 0x1U;
+    ctx->V[x] >>= 1;
+}
+
+void op_8XYE(ChipContext *ctx, u8 x, u8 y)
+{
+    /* Vx <- SHL Vx */
+    // TODO: add via configuration mode this before Vx <- Vy
+
+    // store the MSB in VF
+    ctx->V[0xF] = (ctx->V[x] & 0x8U) >> 7u;
+    ctx->V[x] <<= 1;
 }
 
 void op_ANNN(ChipContext *ctx, u8 n1, u8 n2, u8 n3)
@@ -63,7 +207,7 @@ void op_DXYN(ChipContext *ctx, u8 x, u8 y, u8 n)
             {
 
                 // screen pixel also on - collision
-                if (*screen_pixel == 0xFFFFFFFF)
+                if (*screen_pixel == 0x01)
                 {
                     ctx->V[0xF] = 1;
                 }
@@ -78,6 +222,7 @@ void chip_cycle(ChipContext *ctx)
 {
     u16 opcode;
 
+    // fetch
     opcode = (ctx->memory[ctx->PC] << 8u) | ctx->memory[ctx->PC + 1];
     ctx->PC += 2;
 
@@ -101,7 +246,7 @@ void decode(u16 opcode, ChipContext *ctx)
             break;
 
         case 0xEE:
-            /* 00EE */
+            op_00EE(ctx); // return
             break;
 
         default:
@@ -114,19 +259,19 @@ void decode(u16 opcode, ChipContext *ctx)
         break;
 
     case 0x2:
-        /* 2NNN */
+        op_2NNN(ctx, second, third, fourth); // call subroutine
         break;
 
     case 0x3:
-        /* 3XNN */
+        op_3XNN(ctx, second, third, fourth); // skip if equal
         break;
 
     case 0x4:
-        /* 4XNN */
+        op_4XNN(ctx, second, third, fourth); // skip if not equal
         break;
 
     case 0x5:
-        /* 5XY0 */
+        op_5XY0(ctx, second, third); // skip if Vx == Vy
         break;
 
     case 0x6:
@@ -141,39 +286,39 @@ void decode(u16 opcode, ChipContext *ctx)
         switch (fourth)
         {
         case 0x0:
-            /* 8XY0 */
+            op_8XY0(ctx, second, third); // Vx <- Vy
             break;
 
         case 0x1:
-            /* 8XY1 */
+            op_8XY1(ctx, second, third); // Vx <- Vx OR Vy
             break;
 
         case 0x2:
-            /* 8XY2 */
+            op_8XY2(ctx, second, third); // Vx <- Vx AND Vy
             break;
 
         case 0x3:
-            /* 8XY3 */
+            op_8XY3(ctx, second, third); // Vx <- Vx XOR Vy
             break;
 
         case 0x4:
-            /* 8XY4 */
+            op_8XY4(ctx, second, third); // Vx + Vy, this will affect the carry flag
             break;
 
         case 0x5:
-            /* 8XY5 */
+            op_8XY5(ctx, second, third); // Vx <- Vx - Vy, this will affect the carry flag
             break;
 
         case 0x6:
-            /* 8XY6 */
+            op_8XY6(ctx, second, third); // Vx <- SHR Vx
             break;
 
         case 0x7:
-            /* 8XY7 */
+            op_8XY7(ctx, second, third); // Vx <- Vy - Vx, this will affect the carry flag
             break;
 
         case 0xE:
-            /* 8XYE */
+            op_8XYE(ctx, second, third); // Vx <- SHL Vx
             break;
 
         default:
@@ -182,7 +327,7 @@ void decode(u16 opcode, ChipContext *ctx)
         break;
 
     case 0x9:
-        /* 9XY0 */
+        op_9XY0(ctx, second, third); // skip if Vx != Vy
         break;
 
     case 0xA:
--- src/chip8/logic.h
+++ src/chip8/logic.h
@@ -1,12 +1,31 @@
 #pragma once
 #include "context.h"
 
-void op_00E0(ChipContext *ctx);
-void op_1NNN(ChipContext *ctx, u8 n1, u8 n2, u8 n3);
-void op_6XNN(ChipContext *ctx, u8 x, u8 n1, u8 n2);
-void op_7XNN(ChipContext *ctx, u8 x, u8 n1, u8 n2);
-void op_ANNN(ChipContext *ctx, u8 n1, u8 n2, u8 n3);
-void op_DXYN(ChipContext *ctx, u8 x, u8 y, u8 n);
+void op_00E0(ChipContext *ctx);                      // clear screen
+void op_1NNN(ChipContext *ctx, u8 n1, u8 n2, u8 n3); // jump
+void op_00EE(ChipContext *ctx);                      // return
+void op_2NNN(ChipContext *ctx, u8 n1, u8 n2, u8 n3); // call
+void op_3XNN(ChipContext *ctx, u8 x, u8 n1, u8 n2);  // skip if equal
+void op_4XNN(ChipContext *ctx, u8 x, u8 n1, u8 n2);  // skip if not equal
+void op_5XY0(ChipContext *ctx, u8 x, u8 y);          // skip if Vx == Vy
+void op_9XY0(ChipContext *ctx, u8 x, u8 y);          // skip if Vx != Vy
+void op_6XNN(ChipContext *ctx, u8 x, u8 n1, u8 n2);  // set register vx to the value nn
+void op_7XNN(ChipContext *ctx, u8 x, u8 n1, u8 n2);  // add value nn to vx
+void op_8XY0(ChipContext *ctx, u8 x, u8 y);          // Vx <- Vy
+void op_8XY1(ChipContext *ctx, u8 x, u8 y);          // Vx <- Vx OR Vy
+void op_8XY2(ChipContext *ctx, u8 x, u8 y);          // Vx <- Vx AND Vy
+void op_8XY3(ChipContext *ctx, u8 x, u8 y);          // Vx <- Vx XOR Vy
+void op_8XY4(ChipContext *ctx, u8 x, u8 y);          // Vx <- Vx + Vy, this will affect the carry flag
+void op_8XY5(ChipContext *ctx, u8 x, u8 y);          // Vx <- Vx - Vy, this will affect the carry flag
+void op_8XY7(ChipContext *ctx, u8 x, u8 y);          // Vx <- Vy - Vx, this will affect the carry flag
+void op_8XY6(ChipContext *ctx, u8 x, u8 y);          // Vx <- SHR Vx
+void op_8XYE(ChipContext *ctx, u8 x, u8 y);          // Vx <- SHL Vy
+void op_ANNN(ChipContext *ctx, u8 n1, u8 n2, u8 n3); // set index
+
+void op_BNNN(ChipContext *ctx, u8 n1, u8 n2, u8 n3); // jump with offset
+void op_CXNN(ChipContext *ctx, u8 nx, u8 n1, u8 n2); // random
+
+void op_DXYN(ChipContext *ctx, u8 x, u8 y, u8 n); // draw
 
 void chip_cycle(ChipContext *ctx);
 void decode(u16 opcode, ChipContext *ctx);
