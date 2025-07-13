f6feda23b96ec7390e820f03d281db444d480d3e
dvillas28 <dvillasc@gmail.com>
2025-07-13 00:53:38 -0400

    fix: Key mapping

--- src/chip8/input.c
+++ src/chip8/input.c
@@ -45,22 +45,22 @@ bool process_input(bool quit, SDL_Event e, ChipContext *ctx, bool *debug_mode)
                 break;
 
             case SDLK_1:
-                ctx->keypad[0x0] = 1;
-                ctx->display[0] = !ctx->display[0];
+                ctx->keypad[0x1] = 1;
+                // ctx->display[0] = !ctx->display[0];
                 break;
 
             case SDLK_2:
-                ctx->keypad[0x1] = 1;
-                ctx->display[1] = !ctx->display[1];
+                ctx->keypad[0x2] = 1;
+                // ctx->display[1] = !ctx->display[1];
                 break;
 
             case SDLK_3:
-                ctx->keypad[0x2] = 1;
-                ctx->display[2] = !ctx->display[2];
+                ctx->keypad[0x3] = 1;
+                // ctx->display[2] = !ctx->display[2];
                 break;
 
             case SDLK_4:
-                ctx->keypad[0x3] = 1;
+                ctx->keypad[0xC] = 1;
                 break;
 
             case SDLK_q:
@@ -76,35 +76,35 @@ bool process_input(bool quit, SDL_Event e, ChipContext *ctx, bool *debug_mode)
                 break;
 
             case SDLK_r:
-                ctx->keypad[0x7] = 1;
-                break;
-
-            case SDLK_a:
-                ctx->keypad[0x8] = 1;
-                break;
-
-            case SDLK_s:
-                ctx->keypad[0x9] = 1;
-                break;
-
-            case SDLK_d:
-                ctx->keypad[0xA] = 1;
-                break;
-
-            case SDLK_f:
-                ctx->keypad[0xB] = 1;
-                break;
-
-            case SDLK_z:
-                ctx->keypad[0xC] = 1;
-                break;
-
-            case SDLK_x:
                 ctx->keypad[0xD] = 1;
                 break;
 
-            case SDLK_c:
+            case SDLK_a:
+                ctx->keypad[0x7] = 1;
+                break;
+
+            case SDLK_s:
+                ctx->keypad[0x8] = 1;
+                break;
+
+            case SDLK_d:
+                ctx->keypad[0x9] = 1;
+                break;
+
+            case SDLK_f:
                 ctx->keypad[0xE] = 1;
+                break;
+
+            case SDLK_z:
+                ctx->keypad[0xA] = 1;
+                break;
+
+            case SDLK_x:
+                ctx->keypad[0x0] = 1;
+                break;
+
+            case SDLK_c:
+                ctx->keypad[0xB] = 1;
                 break;
 
             case SDLK_v:
@@ -122,19 +122,19 @@ bool process_input(bool quit, SDL_Event e, ChipContext *ctx, bool *debug_mode)
             {
 
             case SDLK_1:
-                ctx->keypad[0x0] = 0;
-                break;
-
-            case SDLK_2:
                 ctx->keypad[0x1] = 0;
                 break;
 
-            case SDLK_3:
+            case SDLK_2:
                 ctx->keypad[0x2] = 0;
                 break;
 
-            case SDLK_4:
+            case SDLK_3:
                 ctx->keypad[0x3] = 0;
+                break;
+
+            case SDLK_4:
+                ctx->keypad[0xC] = 0;
                 break;
 
             case SDLK_q:
@@ -150,35 +150,35 @@ bool process_input(bool quit, SDL_Event e, ChipContext *ctx, bool *debug_mode)
                 break;
 
             case SDLK_r:
-                ctx->keypad[0x7] = 0;
-                break;
-
-            case SDLK_a:
-                ctx->keypad[0x8] = 0;
-                break;
-
-            case SDLK_s:
-                ctx->keypad[0x9] = 0;
-                break;
-
-            case SDLK_d:
-                ctx->keypad[0xA] = 0;
-                break;
-
-            case SDLK_f:
-                ctx->keypad[0xB] = 0;
-                break;
-
-            case SDLK_z:
-                ctx->keypad[0xC] = 0;
-                break;
-
-            case SDLK_x:
                 ctx->keypad[0xD] = 0;
                 break;
 
-            case SDLK_c:
+            case SDLK_a:
+                ctx->keypad[0x7] = 0;
+                break;
+
+            case SDLK_s:
+                ctx->keypad[0x8] = 0;
+                break;
+
+            case SDLK_d:
+                ctx->keypad[0x9] = 0;
+                break;
+
+            case SDLK_f:
                 ctx->keypad[0xE] = 0;
+                break;
+
+            case SDLK_z:
+                ctx->keypad[0xA] = 0;
+                break;
+
+            case SDLK_x:
+                ctx->keypad[0x0] = 0;
+                break;
+
+            case SDLK_c:
+                ctx->keypad[0xB] = 0;
                 break;
 
             case SDLK_v:
